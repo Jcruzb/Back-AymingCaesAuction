0bc1c1f9f572f92004313bdc62bf8905b1a2ffba
Juan Carlos Cruz <j.cruzb@outlook.com>
2025-03-02 09:28:24 +0100

    Fix:user and auth endpoints

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res, next) => { res.status(HttpStatus.StatusCodes.OK).send('H
 const userRoutes = require('./routes/user.routes')
 app.use('/user', userRoutes)
 const authRoutes = require('./routes/auth.routes')
-app.use('auth', authRoutes)
+app.use('/user', authRoutes)
 const auctionRoutes = require('./routes/auction.routes')
 app.use('/auction', auctionRoutes)
 const bidRoutes = require('./routes/bid.routes')
