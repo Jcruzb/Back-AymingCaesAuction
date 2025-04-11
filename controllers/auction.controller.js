f90557d12fa2663770753e83899eb02b763ca1d5
CRUZ Juan Carlos <jccruz@ayming.com>
2025-04-11 12:38:18 +0200

    Fea: Adding propertis in AuctionSchema

--- controllers/auction.controller.js
+++ controllers/auction.controller.js
@@ -4,164 +4,95 @@ const User = require('../models/User.model')
 const Bid = require('../models/Bid.model')
 const HttpStatus = require('http-status-codes');
 const createError = require('http-errors');
-const { 
-    sendAuctionNotificationEmail, 
-    sendAuctionClosedNotificationEmail, 
-    sendAuctionResultWinnerEmail, 
-    sendAuctionResultTieEmail, 
-    sendAuctionResultNonWinnerEmail  
-  } = require('../config/nodemailer.config');
+const {
+  sendAuctionNotificationEmail,
+  sendAuctionClosedNotificationEmail,
+  sendAuctionResultWinnerEmail,
+  sendAuctionResultTieEmail,
+  sendAuctionResultNonWinnerEmail
+} = require('../config/nodemailer.config');
 
 module.exports.createAuction = (req, res, next) => {
-    Auction.create(req.body)
-      .then(auction => {
-        // Se asume que req.body.project contiene el id del proyecto
-        return Project.findByIdAndUpdate(
-          auction.project,
-          { $push: { auction: auction._id } },
-          { new: true }
-        )
-        .then(updatedProject => {
-          if (!updatedProject) {
-            throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Proyecto no encontrado');
-          }
-          res.status(HttpStatus.StatusCodes.CREATED).json(auction);
-        });
-      })
-      .catch(next);
-  };
+  const {
+    project,
+    durationDays,
+    minBidIncrement = 0.5,
+    notificationConfig = { dailyNotification: true, finalDayNotification: { active: true, frequencyHours: 1 } }
+  } = req.body;
+
+  // Validaciones manuales
+  if (notificationConfig.finalDayNotification?.active && notificationConfig.finalDayNotification?.frequencyHours < 1) {
+    return next(createError(HttpStatus.StatusCodes.BAD_REQUEST, 'La frecuencia de notificación del último día debe ser de al menos 1 hora.'));
+  }
+
+  Auction.create({
+    project,
+    durationDays,
+    minBidIncrement,
+    notificationConfig
+  })
+    .then(auction => {
+      return Project.findByIdAndUpdate(
+        project,
+        { $push: { auction: auction._id } },
+        { new: true }
+      ).then(updatedProject => {
+        if (!updatedProject) {
+          throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Proyecto no encontrado');
+        }
+        res.status(HttpStatus.StatusCodes.CREATED).json(auction);
+      });
+    })
+    .catch(next);
+};
 
 
-  module.exports.getAuctions = (req, res, next) => {
-    Auction.find()
-      .populate({
-        path: 'project',
-        select: 'title savingsGenerated durationDays createdAt closed',
-      })
-      .then(auctions => {
-          res.status(HttpStatus.StatusCodes.OK).json(auctions);
-      })
-      .catch(next);
+
+module.exports.getAuctions = (req, res, next) => {
+  Auction.find()
+    .populate({
+      path: 'project',
+      select: 'title savingsGenerated durationDays createdAt closed',
+    })
+    .then(auctions => {
+      res.status(HttpStatus.StatusCodes.OK).json(auctions);
+    })
+    .catch(next);
 };
 
 module.exports.getAuction = (req, res, next) => {
-    const { id } = req.params;
-    Auction.findById(id)
-        .then(auction => {
-            if (!auction) return res.status(HttpStatus.StatusCodes.NOT_FOUND).send();
-            res.status(HttpStatus.StatusCodes.OK).json(auction);
-        })
-        .catch(next);
+  const { id } = req.params;
+  Auction.findById(id)
+    .then(auction => {
+      if (!auction) return res.status(HttpStatus.StatusCodes.NOT_FOUND).send();
+      res.status(HttpStatus.StatusCodes.OK).json(auction);
+    })
+    .catch(next);
 };
 
 module.exports.updateAuction = (req, res, next) => {
-    const { id } = req.params;
-    Auction.findByIdAndUpdate(id, req.body, { new: true })
-        .then(auction => {
-            if (!auction) return res.status(HttpStatus.StatusCodes.NOT_FOUND).send();
-            res.status(HttpStatus.StatusCodes.OK).json(auction);
-        })
-        .catch(() => next(createError(HttpStatus.StatusCodes.CONFLICT, 'Error updating auction')));
+  const { id } = req.params;
+  Auction.findByIdAndUpdate(id, req.body, { new: true })
+    .then(auction => {
+      if (!auction) return res.status(HttpStatus.StatusCodes.NOT_FOUND).send();
+      res.status(HttpStatus.StatusCodes.OK).json(auction);
+    })
+    .catch(() => next(createError(HttpStatus.StatusCodes.CONFLICT, 'Error updating auction')));
 };
 
 module.exports.getAuctionDetail = (req, res, next) => {
-    const { id } = req.params; // ID de la subasta
-    const user = req.currentUser; // Usuario autenticado
+  const { id } = req.params; // ID de la subasta
+  const user = req.currentUser; // Usuario autenticado
 
-    console.log(user);
+  console.log(user);
 
-    if (user.role === 'administrador') {
-        // ADMINISTRADOR VE TODAS LAS PUJAS con datos completos
-        Auction.findById(id)
-            .populate({
-                path: 'project',
-                select: 'title savingsGenerated durationDays createdAt closed'
-            })
-            .populate({
-                path: 'bids',
-                populate: [
-                    { path: 'client', select: 'name email' },
-                    { path: 'company', select: 'name cif' }
-                ]
-            })
-            .lean()
-            .exec()
-            .then(auction => {
-                if (!auction) {
-                    return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'Subasta no encontrada' });
-                }
-                res.status(HttpStatus.StatusCodes.OK).json(auction);
-            })
-            .catch(next);
-    } else {
-        // USUARIO SOLO VE SU PUJA
-        Bid.findOne({ auction: id, company: user.company })
-            .populate({
-                path: 'auction',
-                populate: {
-                    path: 'project',
-                    select: 'title savingsGenerated durationDays createdAt closed'
-                }
-            })
-            .populate('client', 'name email')
-            .populate('company', 'name cif')
-            .lean()
-            .exec()
-            .then(bid => {
-                if (!bid) {
-                    return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'No tienes una puja registrada en esta subasta' });
-                }
-                res.status(HttpStatus.StatusCodes.OK).json({
-                    auction: bid.auction,
-                    bids: [bid]
-                });
-            })
-            .catch(next);
-    }
-};
-
-
-module.exports.closeAuction = (req, res, next) => {
-    const { id } = req.params;
-    console.log('Cerrando subasta:', id);
-    
+  if (user.role === 'administrador') {
+    // ADMINISTRADOR VE TODAS LAS PUJAS con datos completos
     Auction.findById(id)
-      .then(auction => {
-        if (!auction) {
-          throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Subasta no encontrada');
-        }
-        if (auction.closed) {
-          throw createError(HttpStatus.StatusCodes.BAD_REQUEST, 'La subasta ya está cerrada');
-        }
-        return Auction.findByIdAndUpdate(id, { closed: true }, { new: true });
+      .populate({
+        path: 'project',
+        select: 'title savingsGenerated durationDays createdAt closed'
       })
-      .then(updatedAuction => {
-        return Project.findById(updatedAuction.project)
-          .then(project => {
-            if (!project) {
-              throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Proyecto no encontrado');
-            }
-            // Obtener solo los usuarios con rol "usuario"
-            return User.find({ role: 'usuario' }).then(users => {
-              const emailPromises = users.map(user => {
-                return sendAuctionClosedNotificationEmail(user, project);
-              });
-              return Promise.all(emailPromises).then(() => {
-                res.status(HttpStatus.StatusCodes.OK).json({
-                  message: 'Subasta cerrada exitosamente, notificaciones enviadas',
-                  updatedAuction,
-                });
-              });
-            });
-          });
-      })
-      .catch(next);
-  };
-
-  module.exports.notifyResults = (req, res, next) => {
-    const { id } = req.params; // ID de la subasta
-
-    Auction.findById(id)
       .populate({
         path: 'bids',
         populate: [
@@ -169,82 +100,167 @@ module.exports.closeAuction = (req, res, next) => {
           { path: 'company', select: 'name cif' }
         ]
       })
-      .populate({
-        path: 'project',
-        select: 'title savingsGenerated durationDays createdAt closed'
-      })
       .lean()
       .exec()
       .then(auction => {
-          if (!auction) {
-              return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'Subasta no encontrada' });
-          }
-          
-          const bids = auction.bids;
-          if (!bids || bids.length === 0) {
-              return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'No hay pujas para esta subasta' });
-          }
-
-          // Determinar el mayor precio de puja
-          const maxBidPrice = Math.max(...bids.map(bid => bid.bidPrice));
-          // Determinar los ganadores (podría ser empate)
-          const winners = bids.filter(bid => bid.bidPrice === maxBidPrice);
-          const tie = winners.length > 1;
-
-          // Preparar las promesas para enviar correos según el resultado
-          const emailPromises = bids.map(bid => {
-            if (winners.find(winner => winner._id.toString() === bid._id.toString())) {
-                // Es ganador
-                if (tie) {
-                    return sendAuctionResultTieEmail(bid.client, auction.project, bid);
-                } else {
-                    return sendAuctionResultWinnerEmail(bid.client, auction.project, bid);
-                }
-            } else {
-                return sendAuctionResultNonWinnerEmail(bid.client, auction.project, bid);
-            }
-          });
-
-          // Actualizar la subasta para marcar que se han notificado los resultados
-          Auction.findByIdAndUpdate(id, { resultsNotified: true }, { new: true })
-            .then(updatedAuction => {
-              Promise.all(emailPromises)
-                .then(() => {
-                  res.status(HttpStatus.StatusCodes.OK).json({ message: 'Resultados notificados', auction: updatedAuction });
-                })
-                .catch(err => {
-                  console.error("Error enviando emails de resultados:", err);
-                  res.status(HttpStatus.StatusCodes.OK).json({ message: 'Resultados notificados, pero hubo errores al enviar algunos emails', auction: updatedAuction });
-                });
-            });
+        if (!auction) {
+          return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'Subasta no encontrada' });
+        }
+        res.status(HttpStatus.StatusCodes.OK).json(auction);
       })
       .catch(next);
+  } else {
+    // USUARIO SOLO VE SU PUJA
+    Bid.findOne({ auction: id, company: user.company })
+      .populate({
+        path: 'auction',
+        populate: {
+          path: 'project',
+          select: 'title savingsGenerated durationDays createdAt closed'
+        }
+      })
+      .populate('client', 'name email')
+      .populate('company', 'name cif')
+      .lean()
+      .exec()
+      .then(bid => {
+        if (!bid) {
+          return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'No tienes una puja registrada en esta subasta' });
+        }
+        res.status(HttpStatus.StatusCodes.OK).json({
+          auction: bid.auction,
+          bids: [bid]
+        });
+      })
+      .catch(next);
+  }
+};
+
+
+module.exports.closeAuction = (req, res, next) => {
+  const { id } = req.params;
+  console.log('Cerrando subasta:', id);
+
+  Auction.findById(id)
+    .then(auction => {
+      if (!auction) {
+        throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Subasta no encontrada');
+      }
+      if (auction.closed) {
+        throw createError(HttpStatus.StatusCodes.BAD_REQUEST, 'La subasta ya está cerrada');
+      }
+      return Auction.findByIdAndUpdate(id, { closed: true }, { new: true });
+    })
+    .then(updatedAuction => {
+      return Project.findById(updatedAuction.project)
+        .then(project => {
+          if (!project) {
+            throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Proyecto no encontrado');
+          }
+          // Obtener solo los usuarios con rol "usuario"
+          return User.find({ role: 'usuario' }).then(users => {
+            const emailPromises = users.map(user => {
+              return sendAuctionClosedNotificationEmail(user, project);
+            });
+            return Promise.all(emailPromises).then(() => {
+              res.status(HttpStatus.StatusCodes.OK).json({
+                message: 'Subasta cerrada exitosamente, notificaciones enviadas',
+                updatedAuction,
+              });
+            });
+          });
+        });
+    })
+    .catch(next);
+};
+
+module.exports.notifyResults = (req, res, next) => {
+  const { id } = req.params; // ID de la subasta
+
+  Auction.findById(id)
+    .populate({
+      path: 'bids',
+      populate: [
+        { path: 'client', select: 'name email' },
+        { path: 'company', select: 'name cif' }
+      ]
+    })
+    .populate({
+      path: 'project',
+      select: 'title savingsGenerated durationDays createdAt closed'
+    })
+    .lean()
+    .exec()
+    .then(auction => {
+      if (!auction) {
+        return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'Subasta no encontrada' });
+      }
+
+      const bids = auction.bids;
+      if (!bids || bids.length === 0) {
+        return res.status(HttpStatus.StatusCodes.NOT_FOUND).json({ message: 'No hay pujas para esta subasta' });
+      }
+
+      // Determinar el mayor precio de puja
+      const maxBidPrice = Math.max(...bids.map(bid => bid.bidPrice));
+      // Determinar los ganadores (podría ser empate)
+      const winners = bids.filter(bid => bid.bidPrice === maxBidPrice);
+      const tie = winners.length > 1;
+
+      // Preparar las promesas para enviar correos según el resultado
+      const emailPromises = bids.map(bid => {
+        if (winners.find(winner => winner._id.toString() === bid._id.toString())) {
+          // Es ganador
+          if (tie) {
+            return sendAuctionResultTieEmail(bid.client, auction.project, bid);
+          } else {
+            return sendAuctionResultWinnerEmail(bid.client, auction.project, bid);
+          }
+        } else {
+          return sendAuctionResultNonWinnerEmail(bid.client, auction.project, bid);
+        }
+      });
+
+      // Actualizar la subasta para marcar que se han notificado los resultados
+      Auction.findByIdAndUpdate(id, { resultsNotified: true }, { new: true })
+        .then(updatedAuction => {
+          Promise.all(emailPromises)
+            .then(() => {
+              res.status(HttpStatus.StatusCodes.OK).json({ message: 'Resultados notificados', auction: updatedAuction });
+            })
+            .catch(err => {
+              console.error("Error enviando emails de resultados:", err);
+              res.status(HttpStatus.StatusCodes.OK).json({ message: 'Resultados notificados, pero hubo errores al enviar algunos emails', auction: updatedAuction });
+            });
+        });
+    })
+    .catch(next);
 };
 
 module.exports.launchAuction = (req, res, next) => {
-    const { id } = req.params;
-    // Actualizamos la subasta, estableciendo closed a false para "lanzarla"
-    Auction.findByIdAndUpdate(id, { closed: false, launched: true }, { new: true })
-      .then(auction => {
-        if (!auction) {
-          throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Subasta no encontrada');
-        }
-        // Obtenemos el proyecto asociado a la subasta
-        return Project.findById(auction.project)
-          .then(project => {
-            if (!project) {
-              throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Proyecto no encontrado');
-            }
-            // Obtenemos sólo los usuarios con rol "usuario" para notificar
-            return User.find({ role: 'usuario' }).then(users => {
-              const emailPromises = users.map(user => {
-                return sendAuctionNotificationEmail(user, project);
-              });
-              return Promise.all(emailPromises).then(() => {
-                res.status(HttpStatus.StatusCodes.OK).json({ message: 'Subasta lanzada y notificaciones enviadas', auction });
-              });
+  const { id } = req.params;
+  // Actualizamos la subasta, estableciendo closed a false para "lanzarla"
+  Auction.findByIdAndUpdate(id, { closed: false, launched: true }, { new: true })
+    .then(auction => {
+      if (!auction) {
+        throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Subasta no encontrada');
+      }
+      // Obtenemos el proyecto asociado a la subasta
+      return Project.findById(auction.project)
+        .then(project => {
+          if (!project) {
+            throw createError(HttpStatus.StatusCodes.NOT_FOUND, 'Proyecto no encontrado');
+          }
+          // Obtenemos sólo los usuarios con rol "usuario" para notificar
+          return User.find({ role: 'usuario' }).then(users => {
+            const emailPromises = users.map(user => {
+              return sendAuctionNotificationEmail(user, project);
+            });
+            return Promise.all(emailPromises).then(() => {
+              res.status(HttpStatus.StatusCodes.OK).json({ message: 'Subasta lanzada y notificaciones enviadas', auction });
             });
           });
-      })
-      .catch(next);
-  };
+        });
+    })
+    .catch(next);
+};
--- models/Auction.model.js
+++ models/Auction.model.js
@@ -2,51 +2,44 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AuctionSchema = new Schema({
-    project: {
-        type: Schema.Types.ObjectId,
-        ref: 'Project',
-        required: [true, 'Se requiere asociar un proyecto a la subasta']
+    project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
+    startDate: { type: Date, default: Date.now },
+    durationDays: { type: Number, required: [true, 'Se requiere la duración de la subasta en días'] },
+    launched: { type: Boolean, default: false },
+    closed: { type: Boolean, default: false },
+    bids: [{ type: Schema.Types.ObjectId, ref: 'Bid' }],
+    resultsNotified: { type: Boolean, default: false },
+    isTieBreak: { type: Boolean, default: false },
+    parentAuction: { type: Schema.Types.ObjectId, ref: 'Auction', default: null },
+    eligibleClients: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  
+    // NUEVOS CAMPOS:
+    minBidIncrement: { 
+      type: Number, 
+      default: 0.5, 
+      required: [true, 'Se requiere el incremento mínimo de puja']
     },
-    startDate: {
-        type: Date,
-        default: Date.now
-    },
-    durationDays: {
-        type: Number,
-        required: [true, 'Se requiere la duración de la subasta en días']
-    },
-    launched:{
+  
+    notificationConfig: {
+      dailyNotification: {
         type: Boolean,
-        default:false
-    },
-    closed: {
-        type: Boolean,
-        default: false
-    },
-    bids: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Bid'
-    }],
-    resultsNotified: {
-        type: Boolean,
-        default: false
-    },
-    // Campos para la ronda de desempate
-    isTieBreak: {
-        type: Boolean,
-        default: false
-    },
-    parentAuction: {
-        type: Schema.Types.ObjectId,
-        ref: 'Auction',
-        default: null
-    },
-    eligibleClients: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }]
-}, {
-    timestamps: true
-});
+        default: true
+      },
+      finalDayNotification: {
+        active: { 
+          type: Boolean, 
+          default: true 
+        },
+        frequencyHours: {
+          type: Number,
+          default: 1, // cada 1 hora por defecto
+          min: [1, 'La frecuencia mínima debe ser 1 hora'],
+          max: [12, 'La frecuencia máxima debe ser 12 horas']
+        }
+      }
+    }
+  
+  }, { timestamps: true });
+  
 
 module.exports = mongoose.model('Auction', AuctionSchema);
